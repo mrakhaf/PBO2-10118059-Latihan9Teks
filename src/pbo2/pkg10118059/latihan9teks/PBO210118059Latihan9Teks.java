2491a528fe91df568b09fe35af5cf17b5994beb7
mrakhaf <48128093+mrakhaf@users.noreply.github.com>
2019-09-26 17:12:13 +0700

    Add files via upload

---
+++ src/pbo2/pkg10118059/latihan9teks/PBO210118059Latihan9Teks.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118059.latihan9teks;
+
+/**
+ *
+ * @author 
+ * Nama  : Muhammad Rakha Firdaus
+ * NIM   : 10118059
+ * Kelas : IF-2
+ */
+public class PBO210118059Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil " + newline + "Char : " + tab + " berisi " + c);
+    }
+    
+}
